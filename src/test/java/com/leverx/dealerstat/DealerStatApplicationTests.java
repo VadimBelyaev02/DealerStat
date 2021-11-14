0a5a2a02ca29c7ba08b8cd4136f52fc261ee6f94
= <=>
2021-11-14 16:36:24 +0300

    Database structure and table's diagrams are created

---
+++ src/test/java/com/leverx/dealerstat/DealerStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.dealerstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
