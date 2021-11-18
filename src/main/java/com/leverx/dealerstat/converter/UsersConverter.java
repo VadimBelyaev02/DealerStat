807848a6f57b1624d785257daee842eddec4dce9
= <=>
2021-11-18 14:05:20 +0300

    Created UserConverter

---
+++ src/main/java/com/leverx/dealerstat/converter/UsersConverter.java
@@ -0,0 +1,43 @@
+package com.leverx.dealerstat.converter;
+
+import com.leverx.dealerstat.dto.CommentDTO;
+import com.leverx.dealerstat.dto.ConfirmationDTO;
+import com.leverx.dealerstat.dto.GameObjectDTO;
+import com.leverx.dealerstat.dto.UserDTO;
+import com.leverx.dealerstat.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class UsersConverter {
+
+    public UserDTO convertToDTO(final User user) {
+        final String firstName = user.getFirstName();
+        final String lastName = user.getFirstName();
+        final String password = user.getPassword();
+        final String email = user.getEmail();
+        final Date date = user.getCreatingDate();
+        return UserDTO.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .password(password)
+                .email(email)
+                .build();
+    }
+
+    public User convertToModel(final UserDTO userDTO) {
+        final String firstName = userDTO.getFirstName();
+        final String lastName = userDTO.getLastName();
+        final String password = userDTO.getPassword();
+        final String email = userDTO.getEmail();
+        return User.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .password(password)
+                .email(email)
+                .build();
+    }
+}
